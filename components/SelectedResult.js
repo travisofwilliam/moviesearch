741dec50f3982ef96b92eed9129fd2b2b5148e20
travaScriptES7 <32422093+travaScriptES7@users.noreply.github.com>
2020-09-09 07:05:19 -0700

    cleaned up JSX

--- components/SelectedResult.js
+++ components/SelectedResult.js
@@ -1,19 +1,17 @@
 const SelectedResult = ({ data, back }) => {
   return (
-    <div>
-      <div className="jumbotron">
-        <img src={data.Poster} alt="" />
-        <h1 className="display-4">{data.Title}</h1>
-        <p className="lead">{data.Plot}</p>
-        <hr className="my-4" />
-        <p><strong>Written By: </strong>{data.Writer}</p>
-        <p><strong>Rated: </strong>{data.Rated}</p>
-        <p><strong>Director: </strong>{data.Director}</p>
-        <p><strong>Starring: </strong>{data.Actors}</p>
-        <p className="lead">
-          <a onClick={back} className="btn btn-primary btn-lg" role="button">Back to Search Results</a>
-        </p>
-      </div>
+    <div className="jumbotron">
+      <img src={data.Poster} alt="" />
+      <h1 className="display-4">{data.Title}</h1>
+      <p className="lead">{data.Plot}</p>
+      <hr className="my-4" />
+      <p><strong>Written By: </strong>{data.Writer}</p>
+      <p><strong>Rated: </strong>{data.Rated}</p>
+      <p><strong>Director: </strong>{data.Director}</p>
+      <p><strong>Starring: </strong>{data.Actors}</p>
+      <p className="lead">
+        <a onClick={back} className="btn btn-primary btn-lg" role="button">Back to Search Results</a>
+      </p>
     </div>
   )
 }
